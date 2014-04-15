b4a675ac9240129e25d2bf1a93b7487d1f8b5f7b
otiai10 <otiai10@gmail.com>
2014-04-15 09:51:27 +0900

    Add client skel

--- client.go
+++ client.go
@@ -1,4 +1,10 @@
 package yt2mp3
 
-type Client struct {
+/**
+ * Client to handle `youtube-dl` command
+ * @see https://github.com/rg3/youtube-dl/blob/master/README.md
+ * @example ./youtube-dl {YouTubeURL} [-x|--extract-audio] [--audio-format mp3]
+ */
+type DownloadClient struct {
+    Command string
 }
